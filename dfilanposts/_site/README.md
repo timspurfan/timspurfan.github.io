64a74cef3b2bf5b9f17df4639859a826239a9840
timspurfan <timspurfan@yahoo.com>
2020-03-27 20:09:43 -0400

    Move Dfilan posts out of _posts into dfilanposts

---
+++ dfilanposts/_site/README.md
@@ -0,0 +1,4 @@
+timspurfan.github.io
+================
+
+My website
---
+++ dfilanposts/_site/auto/danielfilan.el
@@ -0,0 +1,10 @@
+(TeX-add-style-hook
+ "danielfilan"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "cundy2018exploring"
+    "agentmodels"
+    "filan2016loss"
+    "everitt2016self"))
+ :bibtex)
+
